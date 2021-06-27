27ce1f4a10c64b3c7a44591942c45a6092bc539a
alan <alan.paulo@radixeng.com.br>
2021-06-27 13:32:01 -0300

    Creating DepartmentDao and SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,19 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
+
 
     }
 }
